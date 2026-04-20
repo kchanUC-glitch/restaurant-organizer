7bd7285db5b45ee0471328a6b1a44e04095edd53
Austin <austin.florence.72@gmail.com>
2026-04-19 22:22:10 -0400

    updated

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include "io.h"
 
 int main() {
+    std::string fileName = "restaurants.txt";
+
+    std::vector<Restaurant> restaurants = getRestaurants(fileName);
+
+    // for (int i = 0; i < restaurants.size(); i++) {
+    //     std::cout << "Name: " << restaurants.at(i).name << "Cuisine: " << restaurants.at(i).cuisine << "Rating: " << restaurants.at(i).rating << "Cost per person: " << restaurants.at(i).costPerPerson;
+    // }
+
     return 0;
 }
